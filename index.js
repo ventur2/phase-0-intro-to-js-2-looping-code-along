28b6a2ca618ab6370dcf8f29f9094f6116b8e73d
rachell <dazzlemevanity@gmail.com>
2022-06-04 14:31:28 -0400

    commit tree 2ea918c868f163e17d8d1385b2ff7864482a16b3

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+// // for (let age = 30; age < 40; age++) {
+// //     console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+//   const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+//  writeCards (['Rae', 'Jay', 'Steph'], Birthday)
+ 
+ function writeCards (name, events){
+    let newArray = []
+
+    for(let i =0; i<name.length; i++){
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${events} gift!`)
+    }
+    return newArray
+    
+ }
+ 
+ 
+function countDown(down){
+    
+
+    while (down >= 0) {
+        console.log (down--)
+    }
+}
+// let count = 0;
